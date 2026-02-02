db56768f260e7dc0760407736de67c78568097cb
Pratik Agre <agrepratik150@gmail.com>
2026-02-02 17:37:32 +0530

    fix register api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -6,30 +6,26 @@ interface User {
   password: string;
 }
 
-const uri = process.env.MONGODB_URI || "";
-
+const uri = process.env.MONGODB_URI as string;
 
 if (!uri) {
-  throw new Error("Please define the MONGO_DB_URI environment variable");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 // Function to connect to MongoDB
 async function connectToDatabase() {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
   const client = new MongoClient(uri);
   await client.connect();
-  return client.db("todo-nextjs-app"); // Replace with your actual database name
+  return client.db("todo-nextjs-app");
 }
 
 export async function POST(request: Request) {
   const { username, password }: User = await request.json();
 
-  // Validate input
   if (!username || !password) {
     return NextResponse.json(
       { error: "Username and password are required" },
-      { status: 400 },
+      { status: 400 }
     );
   }
 
@@ -37,35 +33,26 @@ export async function POST(request: Request) {
     const db = await connectToDatabase();
     const usersCollection = db.collection("users");
 
-    // Check if user already exists
     const existingUser = await usersCollection.findOne({ username });
+
     if (existingUser) {
       return NextResponse.json(
         { error: "User already exists" },
-        { status: 400 },
+        { status: 400 }
       );
     }
 
-    // Create a new user
-    const newUser = { username, password }; // Note: Password is not hashed (not secure)
-    const result = await usersCollection.insertOne(newUser);
+    await usersCollection.insertOne({ username, password });
 
-    if (result.acknowledged) {
-      return NextResponse.json(
-        { message: "User registered successfully", user: { username } },
-        { status: 201 },
-      );
-    } else {
-      return NextResponse.json(
-        { error: "Failed to register user" },
-        { status: 500 },
-      );
-    }
+    return NextResponse.json(
+      { message: "User registered successfully", user: { username } },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error during user registration:", error);
     return NextResponse.json(
       { error: "Internal server error" },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
